063c1d244561a2863604b5cd6a5f62d1cdf975f9
Jordan <jwing421@gmail.com>
2021-06-04 22:46:20 -0700

    gap-filled NWIS data

--- data query/data query.r
+++ data query/data query.r
@@ -1,14 +1,15 @@
 library(tidyverse)
-library(rnoaa) #NOAA GHCND meteorological data queries
+library(rnoaa) # NOAA GHCND meteorological data queries
 library(dataRetrieval) # USGS NWIS hydrologic data queries
+library(baytrends) # interpolation
 
 # NWIS Site Selection-----------------------------------------------------------
 
-US_states <- c('AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA',
-               'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY', 'LA', 'ME', 'MD',
-               'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ',
-               'NM', 'NY', 'NC', 'ND', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC',
-               'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY')
+US_states <- c("AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA",
+               "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD",
+               "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ",
+               "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI", "SC",
+               "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY")
 
 nitrate_sites <- list()
 
@@ -26,7 +27,7 @@ nitrate_sites <- nitrate_sites[lapply(nitrate_sites, nrow) > 0]
 nitrate_sites <- lapply(nitrate_sites, function(x) select(x, -alt_acy_va)) %>%
   bind_rows()
 
-nitr_delin <- read_csv("data query/delineated_sites.csv") %>% #file contains ~19,000 delineated watersheds
+nitr_delin <- read_csv("data query/delineated_sites.csv") %>% # file contains ~19,000 delineated watersheds
   right_join(nitrate_sites, by = c("SITE_NO" = "site_no")) %>%
   filter(!is.na(SQMI))
 
@@ -58,15 +59,16 @@ candidates_lengths <- left_join(data_avail, parm_key, by = c("parm_cd" = "parame
 candidates_meta <- whatNWISsites(siteNumbers = candidates_lengths$site_no)
 
 nwis_data <- readNWISdv(siteNumbers = candidates_meta$site_no, 
-           parameterCd = c('00060',
-                           '99133',
-                           '00010',
-                           '00095',
-                           '00300',
-                           '00400',
-                           '63680'), 
-           statCd = c('00003', '00008'), 
-           startDate = '2010-01-01') 
+           parameterCd = c("00060",
+                           "99133",
+                           "00010",
+                           "00095",
+                           "00300",
+                           "00400",
+                           "63680"), 
+           statCd = c("00003", "00008"), 
+           startDate = "2010-01-01",
+           endDate = Sys.Date() - 2) #to avoid NA values when remote dataset updates
 
 nwis_data %>%
   filter((is.na(X_.YSI._63680_00003) + is.na(X_.YSI._63680_00003) + is.na(X_.HACH._63680_00003)) < 2)
@@ -86,21 +88,89 @@ nwis_tidy <- nwis_data %>%
 
 # NWIS Gap Filling--------------------------------------------------------------
 
-### Plot
+### Check and fix continuity of Date variable
 
 nwis_tidy %>%
-  pivot_longer(cols = water_temp:nitrate) %>%
-  mutate(facet = paste(site_no, name)) %>%
-  filter(value > -1000, name %in% c("water_temp", "discharge")) %>%
-  ggplot(aes(x = Date, y = value)) +
-  geom_point() +
-  facet_wrap(~facet, scales = "free_y", ncol = 4)
+  group_by(site_no) %>%
+  mutate(test = Date - lag(Date) == 1) %>%
+  filter(test != TRUE | is.na(test))
+
+nwis_tidy %>%
+  filter(site_no == "05524500", Date > "2016-10-20", Date < "2016-10-30")
+
+nwis_tidy <- nwis_tidy %>%
+  bind_rows(tibble(site_no = "05524500",
+                   Date = as.Date("2016-10-25"),
+                   water_temp = NA,
+                   discharge =  NA,
+                   spec_cond =  NA,
+                   dissolv_O =  NA,
+                   pH =         NA,
+                   turbidity =  NA,
+                   nitrate =    NA)) %>%
+  arrange(site_no, Date) 
+
+nwis_tidy %>%
+  group_by(site_no) %>%
+  mutate(test = Date - lag(Date) == 1) %>%
+  filter(test != TRUE | is.na(test))
+
+nwis_tidy %>%
+  filter(site_no == "05524500", Date > "2016-10-20", Date < "2016-10-30")
 
 ### Fill discharge gaps
 
+nwis_tidy <- nwis_tidy %>%
+  mutate(discharge_interp = if_else(is.na(discharge),
+                                    true = "linear",
+                                    false = "raw")) %>%
+  
+  # linear interpolation: https://www.rdocumentation.org/packages/baytrends/versions/2.0.5/topics/fillMissing
+  mutate(discharge = fillMissing(discharge, span = 1, max.fill = 7))
+
 ### Fill water temperature gaps
 
+nwis_tidy <- nwis_tidy %>%
+  mutate(water_temp_interp = if_else(is.na(water_temp),
+                                    true = "linear",
+                                    false = "raw")) %>%
+  
+  # same as for discharge
+  mutate(water_temp = fillMissing(water_temp, span = 1, max.fill = 7))
+
+# Now there are some NA values marked linear, to be interpolated by seasonal pattern 
+
+nwis_tidy <- nwis_tidy %>%
+  mutate(julian = as.numeric(format(Date, "%j"))) 
+
+nwis_filled <- nwis_tidy %>%
+  group_by(julian, .add = TRUE) %>%
+  mutate(water_temp_interp = if_else(water_temp_interp == "linear" & is.na(water_temp),
+                                     true = "seasonal",
+                                     false = water_temp_interp)) %>%
+
+  mutate(water_temp = if_else(is.na(water_temp),
+                              true = mean(water_temp, na.rm = TRUE),
+                              false = water_temp))
+
+# Check continuity
+if (sum(is.na(nwis_filled$discharge)) + sum(is.na(nwis_filled$water_temp)) == 0) cat("Data filled successfully")
+
 ### Plot gap-filled data
+
+nwis_filled %>%
+  filter(Date > "2014-06-01") %>%
+  ggplot(aes(x = Date, y = water_temp, color = water_temp_interp)) +
+  geom_point() +
+  facet_wrap(~site_no)
+
+nwis_filled %>%
+  ggplot(aes(x = Date, y = discharge, color = discharge_interp)) +
+  geom_point() +
+  facet_wrap(~site_no) +
+  geom_vline(data = filter(nwis_filled, discharge_interp == "linear"),
+             aes(xintercept = Date), color = "red") +
+  scale_color_manual(values = c("red", "black"))
 
 # NOAA Site Selection-----------------------------------------------------------
 
--- data query/documentation.md
+++ data query/documentation.md
@@ -18,19 +18,23 @@ All 50 states were screened for sites possessing daily measurements (`service = 
 
 ## NWIS Data Query and Gap Filling
 
-Across the 9 sites, the earliest 'dv' data availability of water chemistry variables was around 2014. Data was queried from 2010 onwards to provide sufficient discharge history for our lookback.
+Across the 9 sites, the earliest 'dv' data availability of water chemistry variables was 2013-12-05. Data were queried from 2010 onwards to provide sufficient discharge history for our lookback.
 
-Discharge and water temperature are the only two hydrologic variables that we gap-filled. Water chemistry variables are considered response variables for the time being, and response variables don't necessarily have to be continuous for an LSTM.
+Discharge and water temperature are the only two hydrologic variables that we gap-filled so far. Water chemistry variables are considered response variables for the time being, and response variables don't necessarily have to be continuous for an LSTM.
 
 ### Discharge
 
+Data coverage for discharge was very good: only 9 missing days between 2010-01-01 and 2021-06-04 for all 9 sites combined. Gaps were all only 1-2 days long. These were filled with simple linear interpolation using `baytrends` ([documentation](https://www.rdocumentation.org/packages/baytrends/versions/2.0.5/topics/fillMissing)). A column named "discharge_interp" was added to flag interpolated values.
+
 ### Water Temperature 
+
+Water temperature had many more gaps, some of which were several months in length. We will need to revisit how to fill these gaps, but for now, I filled 7-day-or-shorter gaps with linear interpolation. Longer gaps were filled day-wise with the average of all values collected at that site on that day of the year. These two measures combined filled all gaps, though it should be noted that it filled them all the way from 2010 to the first measured value with a repeating seasonal pattern.
 
 ## GHCND Site Selection
 
 All GHCND stations with data collected before 2010 and after 2019 for one or more variables of interest (listed below) were considered eligible. For each of the 9 NWIS sites, meteorological data were retrieved from the nearest eligible GHCND site.
 
-### meteorological variables (and GHCND abbreviations)
+### Meteorological variables (and GHCND abbreviations)
 
 - Precipitation (PRCP)
 - Snow (SNOW)
